3131f3f1918a00db5837cd8d941fb6287da4c5ee
Sirio <sirioroberto1@hotmail.com>
2023-10-03 13:50:03 -0300

    Created game with 8 attemps, where the user can guess letters and uncover the word. There is no win or lost condition yet

--- main.js
+++ main.js
@@ -18,17 +18,55 @@ function validateUserGuess(userGuess, correctWord) {
   }
 }
 
-function blurWord(correctWord, unBlurredChars = 3) {
-  return correctWord.substring(0, unBlurredChars) + "-".repeat(correctWord.length - unBlurredChars);
+function blurWord(word) {
+  return "-".repeat(word.length);
+}
+function uncoverLetters(blurredWord, letterIndexes, letter) {
+  const blurArray = Array.from(blurredWord);
+  letterIndexes.forEach(i => blurArray[i] = letter);
+  return blurArray.join("");
+}
+
+function findLetterIndexes(word, letter) {
+  const indexes = [];
+
+  let currentIndex = word.indexOf(letter);
+
+  while (currentIndex !== -1) {
+    indexes.push(currentIndex);
+    currentIndex = word.indexOf(letter, currentIndex + 1);
+  }
+
+  return indexes;
+}
+
+function runGame(attempts) {
+  const correctWord = getRandomWord();
+  let blurredWord = blurWord(correctWord);
+
+  do {
+    console.log(blurredWord);
+    const letter = input(`Input a letter: `);
+
+    const letterIndexes = findLetterIndexes(correctWord, letter);
+    if (letterIndexes.length > 0) {
+      blurredWord = uncoverLetters(blurredWord, letterIndexes, letter);
+    } else {
+      console.log("That letter doesn't appear in the word.");
+    }
+
+    console.log();
+    attempts--;
+  } while (attempts > 0);
 }
 
 function main() {
-  console.log("H A N G M A N");
-  const correctWord = getRandomWord();
-  const blurredWord = blurWord(correctWord);
+  console.log("H A N G M A N\n");
+  let attempts = 8;
 
-  let userGuess = input(`Guess the word ${blurredWord}: `);
-  validateUserGuess(userGuess, correctWord);
+  runGame(attempts);
+
+  console.log("Thanks for playing!");
 }
 
 main();
