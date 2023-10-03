42088f8723d11fbe1fd3d4e71d8972fe4f522cac
Sirio <sirioroberto1@hotmail.com>
2023-10-03 11:09:35 -0300

    Implemented possible words that will be picked randomly so users can try to guess it

--- main.js
+++ main.js
@@ -3,12 +3,26 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
-console.log("H A N G M A N");
+const possibleWords = ["python", "java", "swift", "javascript"];
 
-let userGuess = input("Guess the word: ");
-
-if (userGuess === "python") {
-  console.log("You survived!");
-} else {
-  console.log("You lost!");
+function getRandomWord() {
+  const index = Math.floor(Math.random() * possibleWords.length);
+  return possibleWords[index];
 }
+
+function validateUserGuess(userGuess) {
+  if (userGuess === getRandomWord()) {
+    console.log("You survived!");
+  } else {
+    console.log("You lost!");
+  }
+}
+
+function main() {
+  console.log("H A N G M A N");
+
+  let userGuess = input("Guess the word: ");
+  validateUserGuess(userGuess);
+}
+
+main();
