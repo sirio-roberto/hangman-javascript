19c39841ba6bce77e0263d2430bc0301fc8a8eaa
Sirio <sirioroberto1@hotmail.com>
2023-10-03 11:44:42 -0300

    Added method to allow users to see the first 3 charactrs of the hidden word

--- main.js
+++ main.js
@@ -10,19 +10,25 @@ function getRandomWord() {
   return possibleWords[index];
 }
 
-function validateUserGuess(userGuess) {
-  if (userGuess === getRandomWord()) {
+function validateUserGuess(userGuess, correctWord) {
+  if (userGuess === correctWord) {
     console.log("You survived!");
   } else {
     console.log("You lost!");
   }
 }
 
+function blurWord(correctWord, unBlurredChars = 3) {
+  return correctWord.substring(0, unBlurredChars) + "-".repeat(correctWord.length - unBlurredChars);
+}
+
 function main() {
   console.log("H A N G M A N");
+  const correctWord = getRandomWord();
+  const blurredWord = blurWord(correctWord);
 
-  let userGuess = input("Guess the word: ");
-  validateUserGuess(userGuess);
+  let userGuess = input(`Guess the word ${blurredWord}: `);
+  validateUserGuess(userGuess, correctWord);
 }
 
 main();
